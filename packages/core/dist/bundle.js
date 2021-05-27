31ed1bd750b4a9cc7fdfbfbe1d872605dff9f2d8
Parminder Klair <perminder.klair@gmail.com>
2021-05-27 20:40:48 +0530

    update

--- packages/core/dist/bundle.js
+++ packages/core/dist/bundle.js
@@ -19,22 +19,28 @@
           typeof define === 'function' && define.amd ? define(factory) :
           factory();
         }((function () {
-          (function (global, factory) {
-            typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-            typeof define === 'function' && define.amd ? define(['exports'], factory) :
-            (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.zeiqBase = {}));
-          }(undefined, (function (exports) {
-            const cl = (text) => {
-              console.log(text);
-            };
+          (function (factory) {
+            typeof define === 'function' && define.amd ? define(factory) :
+            factory();
+          }((function () {
+            (function (global, factory) {
+              typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+              typeof define === 'function' && define.amd ? define(['exports'], factory) :
+              (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.zeiqBase = {}));
+            }(undefined, (function (exports) {
+              const cl = (text) => {
+                console.log(text);
+              };
 
-            var helpers = { cl };
+              var helpers = { cl };
 
-            var index = { helpers };
+              var index = { helpers };
 
-            exports.utils = index;
+              exports.utils = index;
 
-            Object.defineProperty(exports, '__esModule', { value: true });
+              Object.defineProperty(exports, '__esModule', { value: true });
+
+            })));
 
           })));
 
