3f0077cc33c6399b475f9cb82288c63318a43b79
Parminder Klair <perminder.klair@gmail.com>
2021-06-12 17:06:40 +0530

    Update TextArea.js

--- packages/web/src/atoms/TextArea.js
+++ packages/web/src/atoms/TextArea.js
@@ -4,7 +4,7 @@ const TextArea = ({ name, label, error, className, ...props }) => (
   <textarea
     id={name}
     name={name}
-    className={`w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out ${className}`}
+    className={`w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 py-4 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out ${className}`}
     {...props}
   />
 );
