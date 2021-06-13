c50981cc49adfe572b3c9188559daa613308699c
Parminder Klair <perminder.klair@gmail.com>
2021-06-13 13:56:43 +0530

    fixed file namings

--- packages/mobile/dist/bundle.js
+++ packages/mobile/dist/bundle.js
@@ -119,7 +119,13 @@
 																														typeof define === 'function' && define.amd ? define(factory) :
 																														factory();
 																													}((function () {
-																														console.log('hello world!');
+																														(function (factory) {
+																															typeof define === 'function' && define.amd ? define(factory) :
+																															factory();
+																														}((function () {
+																															console.log('hello world!');
+
+																														})));
 
 																													})));
 
