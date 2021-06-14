1471c2048b109f54f6a5ee6c6df630c20270c6d4
Parminder Klair <perminder.klair@gmail.com>
2021-06-14 23:10:16 +0530

    update

--- packages/mobile/dist/bundle.js
+++ packages/mobile/dist/bundle.js
@@ -123,7 +123,13 @@
 																															typeof define === 'function' && define.amd ? define(factory) :
 																															factory();
 																														}((function () {
-																															console.log('hello world!');
+																															(function (factory) {
+																																typeof define === 'function' && define.amd ? define(factory) :
+																																factory();
+																															}((function () {
+																																console.log('hello world!');
+
+																															})));
 
 																														})));
 
