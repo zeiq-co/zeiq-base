c795660ba5b3f48d245ab0fae69af2f3630fc749
Parminder Klair <perminder.klair@gmail.com>
2021-06-15 19:10:22 +0530

    updated storybook theme

---
+++ packages/web/.storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Zeiq Base',
+  brandUrl: 'https://zeiq.co',
+  brandImage: 'https://www.zeiq.co/assets/images/logo/logo.png',
+});
