dc02ee3ec705e47df125c244a7792d789820b3fb
Parminder Klair <perminder.klair@gmail.com>
2021-06-20 10:05:01 +0530

    improved dark mode

--- packages/web/src/components/atoms/Label.js
+++ packages/web/src/components/atoms/Label.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const Label = ({ children, className, ...props }) => (
   <label
     className={className}
-    tw="leading-7 text-sm text-gray-500 font-medium"
+    tw="leading-7 text-sm text-gray-500 dark:text-gray-300 font-medium"
     {...props}
   >
     {children}
