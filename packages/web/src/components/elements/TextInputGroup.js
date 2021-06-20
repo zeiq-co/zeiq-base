fc94f5a5a119777828b3782b9df70716f94ff67f
Parminder Klair <perminder.klair@gmail.com>
2021-06-20 10:06:55 +0530

    improved dark mode

--- packages/web/src/components/elements/TextInputGroup.js
+++ packages/web/src/components/elements/TextInputGroup.js
@@ -13,7 +13,7 @@ const TextInputGroup = ({ name, label, error, addon, ...props }) => (
       <div tw="flex">
         <Input name={name} {...props} tw="rounded-l" />
         <div tw="w-48 flex items-center justify-center bg-gray-400 rounded-r text-gray-100">
-          .gallery.co
+          {addon}
         </div>
       </div>
     ) : (
